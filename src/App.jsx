0ccb451187d59c4da068533acc9b380b37b1656d
Le Trong Dat <datlt2306@gmail.com>
2022-03-14 12:21:27 +0700

    Buổi 4 - Router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import 'antd/dist/antd.css';
-
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-const { SubMenu } = Menu;
+import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App(){
-  const [current, setCurrent] = useState("mail");
-  const handleClick = e => {
-    setCurrent(e.key)
-  };
+
   return ( 
     <div>
-      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="product" element={<h1>Product Page</h1>}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Menu } from 'antd';
+import  { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+        <Menu mode="horizontal">
+        <Menu.Item>
+          <NavLink to="/">
+            Home Page
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink to="/product">
+            Product Page
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+    </>
+  )
+}
+
+export default Header
