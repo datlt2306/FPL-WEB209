db4c725659f8066016d7c00ffd5618436a589055
Le Trong Dat <datlt2306@gmail.com>
2022-10-31 10:47:13 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function Article() {
+    const seo = {
+        title: "Article Title",
+        description: "Article Description",
+    };
+    const title = "My First Component";
+    const id = 1;
+    const data = ["Components: UI Building Blocks", "Defining a Component", "Using a Component"];
+    return (
+        <article>
+            <h1>{`${title} Le Trong Dat `}</h1>
+            <ol>
+                {data.map((item, index) => (
+                    <li key={index}>
+                        <a href={`/product/${id}`}>{item}</a>
+                    </li>
+                ))}
+            </ol>
+        </article>
+    );
+}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<Article />);
