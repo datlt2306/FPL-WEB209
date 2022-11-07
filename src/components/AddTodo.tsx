813723cc09713c39330ad3d97b88e9686e018461
Le Trong Dat <datlt2306@gmail.com>
2022-11-07 10:47:08 +0700

    get value multi input

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,20 +4,23 @@ type TodoProps = {
     addTodo: (todo: string) => void;
 };
 const AddTodo = (props: TodoProps) => {
-    const [value, setValue] = useState<string>("");
+    const [value, setValue] = useState({});
 
-    const onHandleChange = (e: any) => {
-        setValue(e.target.value);
+    const onHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const event = e.target;
+        console.log(event.value);
+        setValue({ ...value, [event.name]: event.value });
     };
-    const onHandleSubmit = (e: any) => {
+    const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        props.addTodo(value);
+        // props.addTodo(value);
     };
     return (
         <>
-            {value}
+            {JSON.stringify(value)}
             <form onSubmit={onHandleSubmit}>
-                <input type="text" onChange={onHandleChange} />
+                <input type="text" name="name" onChange={onHandleChange} />
+                <input type="text" name="price" onChange={onHandleChange} />
                 <button>Add Todo</button>
             </form>
         </>
