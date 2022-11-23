550491a581863e7251929415bca24b659702d1b8
Le Trong Dat <datlt2306@gmail.com>
2022-11-23 10:38:24 +0700

    update icon

--- src/layouts/LayoutAdmin.tsx
+++ src/layouts/LayoutAdmin.tsx
@@ -14,14 +14,21 @@ const LayoutAdmin = () => {
                 <div className="logo" />
                 <Menu theme="dark" mode="horizontal">
                     <Menu.Item>
-                        <Link to="/admin">Thống kê</Link>
+                        <Link to="/admin">
+                            <LaptopOutlined /> Thống kê
+                        </Link>
                     </Menu.Item>
                     <Menu.Item>
-                        <Link to="/admin/products">Sản phẩm</Link>
+                        <Link to="/admin/products">
+                            <NotificationOutlined />
+                            Sản phẩm
+                        </Link>
                     </Menu.Item>
                     <Menu.SubMenu title="sub menu">
                         <Menu.Item>
-                            <Link to="/admin/products/add">Thêm sản phẩm</Link>
+                            <Link to="/admin/products/add">
+                                <UserOutlined /> Thêm sản phẩm
+                            </Link>
                         </Menu.Item>
                     </Menu.SubMenu>
                 </Menu>
@@ -30,14 +37,21 @@ const LayoutAdmin = () => {
                 <Sider width={200} className="site-layout-background">
                     <Menu>
                         <Menu.Item>
-                            <Link to="/admin">Thống kê</Link>
+                            <Link to="/admin">
+                                <LaptopOutlined /> Thống kê
+                            </Link>
                         </Menu.Item>
                         <Menu.Item>
-                            <Link to="/admin/products">Sản phẩm</Link>
+                            <Link to="/admin/products">
+                                <NotificationOutlined />
+                                Sản phẩm
+                            </Link>
                         </Menu.Item>
                         <Menu.SubMenu title="sub menu">
                             <Menu.Item>
-                                <Link to="/admin/products/add">Thêm sản phẩm</Link>
+                                <Link to="/admin/products/add">
+                                    <UserOutlined /> Thêm sản phẩm
+                                </Link>
                             </Menu.Item>
                         </Menu.SubMenu>
                     </Menu>
