d24ba82d9f2708dd3ffeb1ad394dd6dca3374ce9
Le Trong Dat <datlt2306@gmail.com>
2023-07-11 11:21:54 +0700

    Buổi 4: Update context

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,14 +1,31 @@
 import ProductContext from "@/context/Product.tsx";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const List = () => {
-    const { products, setProducts } = useContext(ProductContext) as any;
+    const { products, fetchProducts, addProduct, deleteProduct, updateProduct } = useContext(
+        ProductContext
+    ) as any;
+    useEffect(() => {
+        fetchProducts();
+    }, []);
     return (
         <div>
-            {products.map((item: any) => item.name)}
-            <button onClick={() => setProducts([...products, { id: 3, name: "Product D" }])}>
-                Add
-            </button>
+            {products.map((item: any) => {
+                return (
+                    <div key={item.id}>
+                        {item.name}
+                        <button onClick={() => deleteProduct(item.id!)}>Delete</button>
+                        <button
+                            onClick={() =>
+                                updateProduct({ name: "Product C updated", id: item.id })
+                            }
+                        >
+                            Edit
+                        </button>
+                    </div>
+                );
+            })}
+            <button onClick={() => addProduct({ name: "Product C" })}>Add</button>
         </div>
     );
 };
--- src/context/Product.tsx
+++ src/context/Product.tsx
@@ -1,14 +1,53 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 const ProductContext = createContext([]);
 
 const ProductProvider = ({ children }: any) => {
-    const [products, setProducts] = useState([
-        { id: 1, name: "Product 1" },
-        { id: 2, name: "Product 2" },
-    ]);
+    const [products, setProducts] = useState([] as any);
+    const [isLoadingProduct, setIsLoadingProduct] = useState(false);
+    const [errorProduct, setErrorProduct] = useState(null);
+
+    const fetchProducts = async () => {
+        setIsLoadingProduct(true);
+        try {
+            const { data } = await axios.get("http://localhost:3000/products");
+            setProducts(data);
+        } catch (error) {
+            setErrorProduct(error.message);
+        } finally {
+            setIsLoadingProduct(false);
+        }
+    };
+    const addProduct = async (product: any) => {
+        const { data } = await axios.post("http://localhost:3000/products", product);
+        setProducts([...products, data]);
+    };
+    const deleteProduct = async (id: any) => {
+        await axios.delete(`http://localhost:3000/products/${id}`);
+        setProducts(products.filter((item: any) => item.id !== id));
+    };
+    const updateProduct = async (product: any) => {
+        // call api
+        await axios.put(`http://localhost:3000/products/${product.id}`, product);
+        //rerender
+        setProducts(products.map((item: any) => (item.id === product.id ? product : item)));
+    };
     return (
-        <ProductContext.Provider value={{ products, setProducts } as any}>
+        <ProductContext.Provider
+            value={
+                {
+                    products,
+                    setProducts,
+                    fetchProducts,
+                    addProduct,
+                    deleteProduct,
+                    updateProduct,
+                    errorProduct,
+                    isLoadingProduct,
+                } as any
+            }
+        >
             {children}
         </ProductContext.Provider>
     );
