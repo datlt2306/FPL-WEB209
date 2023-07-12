9c54c930c4d1d6f784a3999798ca4e2c444f5921
Le Trong Dat <datlt2306@gmail.com>
2023-07-12 13:42:30 +0700

    buổi 5: crud product with context

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,9 +1,11 @@
 import { ProductContext } from "@/context/ProductContext";
 import React, { useContext, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
+import { Button } from "..";
 
 const ProductList = () => {
-    const { products, fetchProducts, isLoading, error } = useContext(ProductContext);
+    const { products, fetchProducts, isLoading, error, addProduct, removeProduct, updateProduct } =
+        useContext(ProductContext);
 
     useEffect(() => {
         fetchProducts();
@@ -16,6 +18,19 @@ const ProductList = () => {
             {products.map((item: any) => {
                 return <div key={item.id}>{item.name}</div>;
             })}
+
+            <Button type="primary" onClick={() => addProduct({ name: "Product C" })}>
+                Add Product
+            </Button>
+            <Button
+                type="primary"
+                onClick={() => updateProduct({ name: "Product C updated ", id: 3 })}
+            >
+                Update Product
+            </Button>
+            <Button type="primary" onClick={() => removeProduct({ id: 3 })}>
+                Delete Product
+            </Button>
         </div>
     );
 };
--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -25,8 +25,55 @@ const ProductProvider = ({ children }: ProductProviderProps) => {
             setIsLoading(false);
         }
     };
+    const addProduct = async (product: any) => {
+        setIsLoading(true);
+        try {
+            await pause(1000);
+            const data = await instance.post(`/products`, product);
+            setProducts([...products, product]);
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    const removeProduct = async (product: any) => {
+        setIsLoading(true);
+        try {
+            await pause(1000);
+            await instance.delete(`/products/${product.id}`);
+            setProducts(products.filter((item) => item.id !== product.id));
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    const updateProduct = async (product: any) => {
+        setIsLoading(true);
+        try {
+            await pause(1000);
+            await instance.put(`/products/${product.id}`, product);
+            setProducts(products.map((item) => (item.id === product.id ? product : item)));
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
-        <ProductContext.Provider value={{ products, isLoading, error, fetchProducts }}>
+        <ProductContext.Provider
+            value={{
+                products,
+                isLoading,
+                error,
+                fetchProducts,
+                removeProduct,
+                updateProduct,
+                addProduct,
+            }}
+        >
             {children}
         </ProductContext.Provider>
     );
