68bbc94b6fba365b682ff6496dd76e4954680358
Le Trong Dat <datlt2306@gmail.com>
2023-07-14 10:44:53 +0700

    Buổi 5: immerjs

--- src/context/Counter.tsx
+++ src/context/Counter.tsx
@@ -1,30 +1,29 @@
 import { createContext, useReducer, useState } from "react";
+import { produce } from "immer";
 
 export const CounterContext = createContext({} as any);
 
 const initialState = {
     count: 10,
+    isLoading: false,
 };
 const reducer = (state: any, action: any) => {
     switch (action.type) {
         case "INCREMENT":
-            return {
-                count: state.count + 1,
-            };
+            state.count++;
+            return;
         case "DECREMENT":
-            return {
-                count: state.count - 1,
-            };
+            state.count--;
+            return;
         case "INCREASE":
-            return {
-                count: state.count + action.payload,
-            };
+            state.count += action.payload;
+            return;
         default:
             return state;
     }
 };
 const CounterProvider = ({ children }: any) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(produce(reducer), initialState);
 
     // const [count, setCount] = useState<number>(0);
 
