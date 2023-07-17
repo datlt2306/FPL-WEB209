6c042f627ad59fce94314f34aea4328a215891f7
Le Trong Dat <datlt2306@gmail.com>
2023-07-17 10:04:16 +0700

    Buổi 7: CRUD Product using userReducer + ImmerJS

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -12,7 +12,7 @@ const ProductList = () => {
             try {
                 const data = await instance.get("/products");
 
-                dispatch({ type: "FETCH_PRODUCTS", payload: data });
+                dispatch({ type: "product/fetchProducts", payload: data });
             } catch (error: any) {
             } finally {
             }
@@ -22,35 +22,29 @@ const ProductList = () => {
     const addProduct = async (product: any) => {
         try {
             const data = await instance.post("/products", product);
-            dispatch({ type: "ADD_PRODUCT", payload: data });
+            dispatch({ type: "product/addProduct", payload: data });
         } catch (error: any) {
         } finally {
         }
     };
-    // const removeProduct = async (product: any) => {
-    //     setIsLoading(true);
-    //     await pause(1000);
-    //     try {
-    //         await instance.delete(`/products/${product.id}`);
-    //         setProducts(products.filter((item) => item.id !== product.id));
-    //     } catch (error: any) {
-    //         setError(error.message);
-    //     } finally {
-    //         setIsLoading(false);
-    //     }
-    // };
-    // const updateProduct = async (product: any) => {
-    //     setIsLoading(true);
-    //     await pause(1000);
-    //     try {
-    //         await instance.put(`/products/${product.id}`, product);
-    //         setProducts(products.map((item) => (item.id === product.id ? product : item)));
-    //     } catch (error: any) {
-    //         setError(error.message);
-    //     } finally {
-    //         setIsLoading(false);
-    //     }
-    // };
+    const removeProduct = async (id: any) => {
+        try {
+            await instance.delete(`/products/${id}`);
+            dispatch({ type: "product/deleteProduct", payload: id });
+        } catch (error: any) {
+        } finally {
+        }
+    };
+    const updateProduct = async (product: any) => {
+        try {
+            const data = await instance.put(`/products/${product.id}`, product);
+
+            console.log(data);
+            dispatch({ type: "product/updateProduct", payload: data });
+        } catch (error: any) {
+        } finally {
+        }
+    };
     if (state.isLoading) return <Skeleton count={3} />;
     if (state.error) return <div>{state.error}</div>;
     return (
@@ -60,11 +54,11 @@ const ProductList = () => {
             ))}
 
             <Button onClick={() => addProduct({ name: "Product Added 1" })}>Thêm</Button>
-            {/* 
+
             <Button onClick={() => updateProduct({ name: "Product Updated", id: 4 })}>
                 Updated
             </Button>
-            <Button onClick={() => removeProduct({ id: 4 })}>Delete</Button> */}
+            <Button onClick={() => removeProduct(4)}>Delete</Button>
         </div>
     );
 };
--- src/reducers/Product.ts
+++ src/reducers/Product.ts
@@ -3,12 +3,22 @@
 
 export const productReducer = (state: any, action: any) => {
     switch (action.type) {
-        case "FETCH_PRODUCTS":
+        case "product/fetchProducts":
             state.products = action.payload;
-            return;
-        case "ADD_PRODUCT":
+            break;
+        case "product/addProduct":
             state.products.push(action.payload);
-            return;
+            break;
+        case "product/updateProduct":
+            const product = action.payload;
+            console.log('product', product);
+            // state.products[action.payload.id] = action.payload;
+            state.products = state.products.map((item: any) => item.id === product.id ? product : item);
+            break;
+        case "product/deleteProduct":
+            const id = action.payload;
+            state.products = state.products.filter((item: any) => item.id !== id);
+            break;
         default:
             return state;
     }
