81fcb26fea75503c993bcebecf796bf6508fe6be
Le Trong Dat <datlt2306@gmail.com>
2023-07-19 10:45:28 +0700

    Buổi 8 : fix warning redux dev tool

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,11 @@ import { counterReducer } from '@/reducers/Counter';
 import { productReducer } from '@/reducers/Product';
 import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
+
 const composeEnhancers =
-    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
             // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
         })
         : compose;
