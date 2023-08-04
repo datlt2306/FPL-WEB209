282b12fe3d8bb096f52ef641d85eec8af5c9d75b
Le Trong Dat <datlt2306@gmail.com>
2023-08-04 09:51:02 +0700

    Buổi 15: Xóa tất cả những gì không liên quan

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-import Form from "./Form";
-import Input from "./Input";
-import Item from "./Item";
-import List from "./List";
-
-export { Button, Form, Input, Item, List }
