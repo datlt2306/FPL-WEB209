b755e5043f8d6c963071a58396a43e5368f306b0
Le Trong Dat <datlt2306@gmail.com>
2023-08-09 13:06:42 +0700

    private router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
+import { createBrowserRouter, Navigate, useNavigate, Outlet } from "react-router-dom";
 import LayoutAdmin from "./components/layouts/LayoutAdmin";
 import Dashboard from "./pages/admin/dashboard";
 import AdminProduct from "./pages/admin/product";
@@ -6,7 +6,18 @@ import AdminProductAdd from "./pages/admin/product/add";
 import AdminProductEdit from "./pages/admin/product/edit";
 import Signup from "./pages/auth/signup";
 import Signin from "./pages/auth/signin";
+import { useEffect } from "react";
+const PrivateRoute = ({ isAuth }: any) => {
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!isAuth) {
+            navigate("/signin");
+        }
+    }, [isAuth]);
+
+    return isAuth ? <Outlet /> : <Navigate to="/signin" />;
+};
 export const router = createBrowserRouter([
     {
         path: "/",
@@ -22,13 +33,18 @@ export const router = createBrowserRouter([
     },
     {
         path: "/admin",
-        element: <LayoutAdmin />,
+        element: <PrivateRoute isAuth={true} />,
         children: [
-            { index: true, element: <Navigate to="dashboard" /> },
-            { path: "dashboard", element: <Dashboard /> },
-            { path: "product", element: <AdminProduct /> },
-            { path: "product/add", element: <AdminProductAdd /> },
-            { path: "product/:idProduct/edit", element: <AdminProductEdit /> },
+            {
+                element: <LayoutAdmin />,
+                children: [
+                    { index: true, element: <Navigate to="dashboard" /> },
+                    { path: "dashboard", element: <Dashboard /> },
+                    { path: "product", element: <AdminProduct /> },
+                    { path: "product/add", element: <AdminProductAdd /> },
+                    { path: "product/:idProduct/edit", element: <AdminProductEdit /> },
+                ],
+            },
         ],
     },
     { path: "*", element: "Not Found Page" },
