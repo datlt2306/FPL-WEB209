64832f0087dd3bd54d2c5c77f0d55ac5b7e930f9
Le Trong Dat <datlt2306@gmail.com>
2023-11-01 14:19:37 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-const dataFake = [
-    { id: 1, name: 'Product A', price: 100 },
-    { id: 2, name: 'Product A', price: 100 },
-    { id: 3, name: 'Product A', price: 100 }
-]
-type Product = {
-    id: number
-    name: string
-    price: number
-}
-type ProductListProps = {
-    products: Product[]
-}
-const ProductList = ({ products }: ProductListProps) => {
-    if (!products) return <>Không có sản phẩm nào</>
-    return (
-        <>
-            {products.map((item, index) => (
-                <div key={index}>{item.name}</div>
-            ))}
-        </>
-    )
-}
-function App() {
-    const [products] = useState<Product[]>(dataFake)
-    return (
-        <>
-            <ProductList products={products} />
-        </>
-    )
-}
+function App() {}
 
 export default App
