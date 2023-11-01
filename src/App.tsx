f931098953460a680fbb4ce8ca17a370e506f7f7
Le Trong Dat <datlt2306@gmail.com>
2023-11-01 13:05:16 +0700

    Buổi 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-    var [count, setCount] = React.useState(0)
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        ;(async () => {
+            try {
+                setLoading(true)
+                const data = await (await fetch('http://localhost:3000/products')).json()
+                setProducts(data)
+            } catch (error) {
+                setError((error as Error).message)
+            } finally {
+                setLoading(false)
+            }
+        })()
+    }, [])
+    if (error) return <div>{error}</div>
+    if (loading) return <div>...loading</div>
+    return <>{products?.map((product: any, index) => <div key={index}>{product.name}</div>)}</>
 }
 
 export default App
