80894444c2189c4d87953a16985e536ee2d60ce7
Le Trong Dat <datlt2306@gmail.com>
2023-11-01 15:14:40 +0700

    Buổi 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { IProduct } from './interfaces/Product'
 function App() {
-    return <>WD18101 update</>
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+
+    useEffect(() => {
+        ;(async () => {
+            try {
+                setIsLoading(true)
+                const data = await (await fetch(`http://localhost:3000/products`)).json()
+                setProducts(data)
+            } catch (error) {
+                console.log('[GET_PRODUCTS_ERROR]', error)
+                setError((error as Error).message)
+            } finally {
+                setIsLoading(false)
+            }
+        })()
+    }, [])
+
+    if (error) return <div>{error}</div>
+    if (isLoading) return <div>Loading...</div>
+    return <>{products?.map((product, index) => <div key={index}>{product.name}</div>)}</>
 }
 
 export default App
