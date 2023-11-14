3eaaf23d629bb4b04533bcd4967bba9ea8b13d2a
Le Trong Dat <datlt2306@gmail.com>
2023-11-14 15:31:16 +0700

    Buổi 5: Edit product with react-query

---
+++ src/components/ProductItem/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { IProduct } from '../../interfaces/Product'
+import { useMutation, useQueryClient } from 'react-query'
+
+type ProductItemProps = {
+    item: IProduct
+}
+
+const ProductItem = ({ item }: ProductItemProps) => {
+    const queryClient = useQueryClient()
+    const [productEditId, setProductEditId] = useState<number | null>(null)
+    const [productEdit, setProductEdit] = useState({} as IProduct)
+    const mutation = useMutation({
+        mutationFn: (product: IProduct) =>
+            fetch(`http://localhost:3000/products/${product.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            }),
+        onSuccess: () => {
+            setProductEditId(null)
+            queryClient.invalidateQueries({
+                queryKey: ['PRODUCT_KEY']
+            })
+        }
+    })
+    const onInput = (e: any) => {
+        const { name, value } = e.target
+        setProductEdit({
+            ...productEdit,
+            [name]: value
+        })
+    }
+    const onSubmit = async (e: any) => {
+        e.preventDefault()
+        mutation.mutate(productEdit)
+    }
+    const handleClick = (item: IProduct) => {
+        setProductEditId(item.id!)
+        setProductEdit(item)
+    }
+    return (
+        <div>
+            {productEditId === item.id ? (
+                <form onSubmit={onSubmit}>
+                    <input type='text' name='name' defaultValue={productEdit?.name} onInput={onInput} />
+                    <input type='text' name='price' defaultValue={productEdit?.price} onInput={onInput} />
+                    <button>Lưu</button>
+                    <button onClick={() => setProductEditId(null)}>Hủy</button>
+                </form>
+            ) : (
+                <>
+                    {item.name} - {item.price}
+                    <button onClick={() => handleClick(item)}>Edit</button>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default ProductItem
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,81 +1,23 @@
-import { useState } from 'react'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { useQuery } from 'react-query'
 import { IProduct } from '../../interfaces/Product'
+import ProductItem from '../ProductItem'
 
 const ProductList = () => {
-    const queryClient = useQueryClient()
-    const [editingProductId, setEditingProductId] = useState<number | null>(null)
-    const [editingProduct, setEditingProduct] = useState<IProduct | null>(null)
     const {
         data: products,
         isLoading,
         isError
-    } = useQuery(['PRODUCT_KEY'], async () => {
-        const response = await fetch(`http://localhost:3000/products`)
-        const data = await response.json()
-        return data
-    })
-    const mutation = useMutation({
-        mutationFn: (product: IProduct) =>
-            fetch(`http://localhost:3000/products/${product.id}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(product)
-            }),
-        onSuccess: () => {
-            queryClient.invalidateQueries('PRODUCT_KEY')
-            setEditingProductId(null)
+    } = useQuery({
+        queryKey: ['PRODUCT_KEY'],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:3000/products`)
+            const data = await response.json()
+            return data
         }
     })
-    const onInput = (e: any) => {
-        const { name, value } = e.target
-        setEditingProduct(
-            (prevEditingProduct) =>
-                ({
-                    ...prevEditingProduct,
-                    [name]: value
-                }) as IProduct
-        )
-    }
-    const onSubmit = async (e: any) => {
-        e.preventDefault()
-        mutation.mutate(editingProduct as any)
-    }
-    const handleEditClick = (product: IProduct) => {
-        setEditingProductId(product.id!)
-        setEditingProduct(product)
-    }
 
     if (isLoading) return <>Loading...</>
     if (isError) return <div>Error.</div>
-    return (
-        <>
-            {products?.map((item: IProduct, index: number) => (
-                <div key={index}>
-                    {editingProductId === item.id ? (
-                        <>
-                            <form onSubmit={onSubmit}>
-                                <input type='text' defaultValue={editingProduct?.name} name='name' onInput={onInput} />
-                                <input
-                                    type='text'
-                                    defaultValue={editingProduct?.price}
-                                    name='price'
-                                    onInput={onInput}
-                                />
-                                <button>Save</button>
-                            </form>
-                        </>
-                    ) : (
-                        <>
-                            {item.name} - {item.price}
-                            <button onClick={() => handleEditClick(item)}>Edit</button>
-                        </>
-                    )}
-                </div>
-            ))}
-        </>
-    )
+    return <>{products?.map((item: IProduct, index: number) => <ProductItem key={index} item={item} />)}</>
 }
 export default ProductList
