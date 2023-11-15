db8db96715d9f7eaab6a871a014fcbc507685158
Le Trong Dat <datlt2306@gmail.com>
2023-11-15 13:24:54 +0700

    Buổi 8: Data table

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
-import { IProduct } from '../interfaces/Product'
-import { getProducts, updateProduct } from '../apis/product'
-import ProductItem from './ProductItem'
+import { useQuery } from 'react-query'
+import { getProducts } from '../apis/product'
+import { ColumnDef } from '@tanstack/react-table'
+import { IProduct } from '@/interfaces/Product'
+import { DataTable } from './DataTable'
+import { Button } from './ui/button'
+
+export const columns: ColumnDef<IProduct>[] = [
+    {
+        accessorKey: 'name',
+        header: () => <span className='font-bold'>Tên sản phẩm</span>
+    },
+    {
+        accessorKey: 'price',
+        header: 'Giá'
+    },
+    {
+        accessorKey: '',
+        header: 'Hành động',
+        cell: ({ row }) => {
+            return (
+                <>
+                    <Button onClick={() => console.log(row?.original.id)}>Xóa</Button>
+                </>
+            )
+        }
+    }
+]
 
 const List = () => {
     const {
@@ -22,7 +45,11 @@ const List = () => {
     if (isLoading) return <div>Loading...</div>
     if (isError) return <div>Error...</div>
 
-    return <div>{products?.map((product: any, index: any) => <ProductItem key={index} product={product} />)}</div>
+    // return <div>{products?.map((product: any, index: any) => <ProductItem key={index} product={product} />)}</div>
+
+    // return <div>{products?.map((product: any, index: any) => <div key={index}>{product?.name}</div>)}</div>
+
+    return <DataTable columns={columns} data={products} />
 }
 
 export default List
