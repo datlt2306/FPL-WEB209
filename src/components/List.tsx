7300465f03638af6574596df93c40c05be547b25
Le Trong Dat <datlt2306@gmail.com>
2023-11-15 15:03:22 +0700

    Buổi 8: Sử dụng database trong shadcn-ui

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,14 +1,59 @@
 import { useQuery } from 'react-query'
 import { getAll } from '../api/product'
-import { IProduct } from '../interfaces/Product'
-import ProductItem from './ProductItem'
+import { ColumnDef } from '@tanstack/react-table'
+import { IProduct } from '@/interfaces/Product'
+import { DataTable } from './DataTable'
+import { Button } from './ui/button'
+import { formatPrice } from '@/lib/utils'
+import { MoreHorizontal, Pencil } from 'lucide-react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
+const columns: ColumnDef<IProduct>[] = [
+    {
+        accessorKey: 'name',
+        header: () => <span className='text-red-500'>Tên sản phẩm</span>
+    },
+    {
+        accessorKey: 'price',
+        header: 'Giá sản phẩm',
+        cell: ({ row }) => {
+            const formattedPrice = formatPrice(row.getValue('price') || 0)
+            return <span dangerouslySetInnerHTML={{ __html: formattedPrice }} />
+        }
+    },
+    {
+        id: 'action',
+        cell: ({ row: { original } }) => {
+            return (
+                <>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant='ghost' className='h-8 w-8 p-0'>
+                                <span className='sr-only'>Open menu</span>
+                                <MoreHorizontal className='h-4 w-4' />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align='end'>
+                            <Button onClick={() => console.log(original.id)}>
+                                <Pencil /> Xóa
+                            </Button>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </>
+            )
+        }
+    }
+]
 
 const List = () => {
-    const {
-        isLoading,
-        isError,
-        data: products
-    } = useQuery({
+    const { isLoading, isError, data } = useQuery({
         queryKey: 'PRODUCTS_KEY',
         queryFn: () => getAll()
     })
@@ -18,7 +63,8 @@ const List = () => {
     return (
         <div>
             <h2>Danh sách</h2>
-            {products?.map((product: IProduct, index: number) => <ProductItem product={product} key={index} />)}
+            <DataTable columns={columns} data={data} />
+            {/* {products?.map((product: IProduct, index: number) => <ProductItem product={product} key={index} />)} */}
         </div>
     )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+export const formatPrice = (price: number) => {
+    const formattedPrice = Intl.NumberFormat('vi-VN', {
+        style: 'currency',
+        currency: 'VND',
+        currencyDisplay: 'code'
+    }).format(price)
+    const formattedPriceWithoutVND = formattedPrice.replace('VND', '')
+    return formattedPriceWithoutVND + '<sup>đ</sup>'
 }
