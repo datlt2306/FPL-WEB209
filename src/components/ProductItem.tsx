a58d0d261eafbc76f73f6757361cd5acb6dde212
Le Trong Dat <datlt2306@gmail.com>
2023-11-15 14:40:00 +0700

    Buổi 8: Edit Product sử dụng react-hook-form

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -3,52 +3,88 @@ import { IProduct } from '../interfaces/Product'
 import { useMutation, useQueryClient } from 'react-query'
 import { edit } from '../api/product'
 import { Button } from './ui/button'
+import joi from 'joi'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { joiResolver } from '@hookform/resolvers/joi'
+import { Form, FormControl, FormField, FormItem } from './ui/form'
+import { Input } from './ui/input'
 
 type ProductItemProps = {
     product: IProduct
 }
+type formInputType = {
+    name: string
+    price: number
+}
+const formAddSchema = joi.object({
+    name: joi.string().required(),
+    price: joi.number()
+})
 
 const ProductItem = ({ product }: ProductItemProps) => {
     const queryClient = useQueryClient()
     const [editProductId, setEditProductId] = useState<number | null>(null)
     const [editProduct, setEditProduct] = useState({} as IProduct)
+
+    const form = useForm<formInputType>({
+        resolver: joiResolver(formAddSchema),
+        defaultValues: {
+            name: '',
+            price: 0
+        }
+    })
     const mutation = useMutation({
         mutationFn: (product: IProduct) => edit(product),
         onSuccess: () => {
             setEditProductId(null)
+            setEditProduct({} as IProduct)
             queryClient.invalidateQueries({
                 queryKey: ['PRODUCTS_KEY']
             })
         }
     })
+
+    const onSubmit: SubmitHandler<formInputType> = (values) => {
+        mutation.mutate({ ...values, id: editProduct.id! })
+    }
     const handleClick = (product: IProduct) => {
         setEditProductId(product.id!)
         setEditProduct(product)
-    }
-
-    const onChange = (e: any) => {
-        const { name, value } = e.target
-        // computed property name
-        setEditProduct((prevState) => ({
-            ...prevState,
-            [name]: value
-        }))
-    }
-    const onSubmit = (e: any) => {
-        e.preventDefault()
-        mutation.mutate(editProduct)
+        form.reset({
+            name: product.name,
+            price: product.price
+        })
     }
     return (
         <div>
             {editProductId === product.id ? (
-                <form onSubmit={onSubmit}>
-                    <input type='text' name='name' defaultValue={editProduct?.name} onChange={onChange} />
-                    <input type='text' name='price' defaultValue={editProduct?.price} onChange={onChange} />
-                    <button>Cập nhật</button>
-                    <Button variant='default' onClick={() => setEditProductId(null)}>
-                        Thoát
-                    </Button>
-                </form>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name='name'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input placeholder='Tên sản phẩm' {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='price'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input placeholder='Giá sản phẩm' {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <Button type='submit'>Lưu</Button>
+                    </form>
+                </Form>
             ) : (
                 <>
                     {product.name} - {product.price}{' '}
