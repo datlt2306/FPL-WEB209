546fb8f59601c5fa6ea46a437ed5ad92f3cba7ef
Le Trong Dat <datlt2306@gmail.com>
2023-11-16 15:39:32 +0700

    Buổi 6: Data Table

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,8 +1,47 @@
-import { useQuery } from 'react-query'
-import { IProduct } from '../../interfaces/Product'
-import ProductItem from '../ProductItem'
-import { getProducts } from '../../api/product'
 import { Skeleton } from '@/components/ui/skeleton'
+import { ColumnDef } from '@tanstack/react-table'
+import { useQuery } from 'react-query'
+import { getProducts } from '../../api/product'
+import { IProduct } from '../../interfaces/Product'
+import { DataTable } from './DataTable'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip'
+import { BsFillQuestionCircleFill } from 'react-icons/bs'
+import { Button } from '../ui/button'
+export const columns: ColumnDef<IProduct>[] = [
+    {
+        accessorKey: 'name',
+        header: () => (
+            <div className='text-red-500'>
+                Tên sản phẩm
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <BsFillQuestionCircleFill />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Nội dung</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </div>
+        )
+    },
+    {
+        accessorKey: 'price',
+        header: 'Giá sản phẩm'
+    },
+    {
+        id: 'action',
+        cell: ({ row }) => {
+            const id = row?.original?.id
+            return (
+                <>
+                    <Button onClick={() => console.log(id)}>Xóa</Button>
+                </>
+            )
+        }
+    }
+]
 
 const ProductList = () => {
     const { data, isLoading, isError } = useQuery({
@@ -17,6 +56,11 @@ const ProductList = () => {
             </>
         )
     if (isError) return <div>Error.</div>
-    return <>{data?.data?.map((item: IProduct, index: number) => <ProductItem key={index} item={item} />)}</>
+    return (
+        <div>
+            <h2>Quản lý sản phẩm</h2>
+            <DataTable columns={columns} data={data?.data} />
+        </div>
+    )
 }
 export default ProductList
