e328fdc545d4898996d890869c7b6e5f9d20c966
Le Trong Dat <datlt2306@gmail.com>
2023-11-16 15:19:23 +0700

    Buổi 6: Skeleton

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -2,13 +2,20 @@ import { useQuery } from 'react-query'
 import { IProduct } from '../../interfaces/Product'
 import ProductItem from '../ProductItem'
 import { getProducts } from '../../api/product'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const ProductList = () => {
     const { data, isLoading, isError } = useQuery({
         queryKey: ['PRODUCT_KEY'],
         queryFn: () => getProducts()
     })
-    if (isLoading) return <>Loading...</>
+    if (isLoading)
+        return (
+            <>
+                <Skeleton className='w-[200px] h-[20px] rounded-full' />
+                <Skeleton className='w-[100px] h-[20px] rounded-full' />
+            </>
+        )
     if (isError) return <div>Error.</div>
     return <>{data?.data?.map((item: IProduct, index: number) => <ProductItem key={index} item={item} />)}</>
 }
