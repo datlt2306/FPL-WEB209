1a22c984aa008e036747f22921f938408795c7d7
Le Trong Dat <datlt2306@gmail.com>
2023-11-21 15:12:12 +0700

    Buổi 9: Edit Product

--- src/components/ProductAdd/index.tsx
+++ src/components/ProductAdd/index.tsx
@@ -1,53 +1,12 @@
-import { joiResolver } from '@hookform/resolvers/joi'
-import { AiFillPlusSquare } from 'react-icons/ai'
-import { addProduct } from '@/api/product'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { IProduct } from '@/interfaces/Product'
-import joi from 'joi'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { useMutation, useQueryClient } from 'react-query'
-import { useToast } from '../ui/use-toast'
 import { useProductMutation } from '@/hooks/useProductMutation'
+import { AiFillPlusSquare } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
+import { useToast } from '../ui/use-toast'
 
-type FormControlType = {
-    name: string
-    price: number
-}
-const formSchema = joi.object({
-    name: joi.string().required(),
-    price: joi.number()
-})
 const ProductAdd = () => {
-    // const queryClient = useQueryClient()
-    // const { toast } = useToast()
-
-    // const form = useForm<FormControlType>({
-    //     resolver: joiResolver(formSchema),
-    //     defaultValues: {
-    //         name: '',
-    //         price: 0
-    //     }
-    // })
-    // const mutation = useMutation({
-    //     mutationFn: (product: IProduct) => addProduct(product),
-    //     onSuccess: () => {
-    //         toast({
-    //             variant: 'success',
-    //             title: 'Chúc mừng bạn!',
-    //             description: 'Bạn đã thêm sản phẩm thành công'
-    //         })
-    //         form.reset()
-    //         queryClient.invalidateQueries('PRODUCT_KEY')
-    //     }
-    // })
-
-    // const onSubmit: SubmitHandler<FormControlType> = (data) => {
-    //     mutation.mutate(data)
-    // }
-
     const { toast } = useToast()
     const navigate = useNavigate()
     const { form, onSubmit } = useProductMutation({
