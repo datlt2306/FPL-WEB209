c869828c5f9102b27adc46fca5b84a1d336b541a
datlt2306 <datlt2306@gmail.com>
2024-03-04 10:22:55 +0700

    Buổi 1 - commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -32,3 +32,4 @@ function App() {
 }
 
 export default App
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import React from 'react'
+
+const myName: string = 'Dat';
+const myAge: number = 40;
+const isStatus: boolean = false;
+const myInfo: { name: string, age: number, status: boolean} = {
+  name: 'Dat',
+  age: 40,
+  status: false
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className="content">
+  <h1>myName: {myName}</h1>
+  <h1>myAge: {myAge}</h1>
+  <h2>isStatus: {isStatus ? 'Đã chết': 'Ế'}</h2>
+  <h2>myInfo: { myInfo.name} - {myInfo.age}</h2>
+</div>
 )
