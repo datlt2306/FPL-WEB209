298af7f0258a8e6f04f13803d922ed73336a15a1
datlt2306 <datlt2306@gmail.com>
2024-03-04 08:44:31 +0700

    Buổi 1

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+const root = document.getElementById('root')!;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const myName = "Dat";
+const myAge = 40;
+const isMarried = true;
+const myInfo = {
+  name: "Dat",
+  age: 20,
+  hasChild: true
+}
+ReactDOM.createRoot(root).render(<>
+  <h1>My Name: {myName}</h1>
+  <h1>My Age: {myAge}</h1>
+  <h1>Status: {isMarried ? 'Đã chết': 'Ế'}</h1>
+  <h1>Info {myInfo.name} - {myInfo.age}</h1>
+</>);
+
+
+  
