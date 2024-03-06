3f0965b10dd42ac22eeccef6bec5a4bf564125dd
datlt2306 <datlt2306@gmail.com>
2024-03-06 09:38:09 +0700

    Commit nhầm

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import Product from "./components/Product";
-import ShowName from "./components/ShowName";
-import { IProduct } from "./interfaces/name";
 
 function App() {
-    const products: IProduct[] = [
-        { id: 1, name: "Product A", price: 200 }, // item
-        { id: 2, name: "Product B", price: 300 }, // item
-        { id: 3, name: "Product C", price: 400 }, // item
-    ];
-    return (
-        <>
-            {products.map((item: IProduct, index) => {
-                return <Product item={item} />;
-            })}
-            <ShowName name="Ahihi" />
-        </>
-    );
+    return <></>;
 }
 
 export default App;
--- src/components/ShowName.tsx
+++
@@ -1,6 +0,0 @@
-import { propsType } from "../interfaces/name";
-
-function ShowName(props: propsType) {
-    return <h1>Hello Dat {props.name}</h1>;
-}
-export default ShowName;
