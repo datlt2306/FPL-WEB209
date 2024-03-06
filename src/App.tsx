c90a2435ac58465d681cd436caaacd20a69f30a5
datlt2306 <datlt2306@gmail.com>
2024-03-06 12:12:17 +0700

    WD18305 - Buổi 2 - commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Product from "./components/Product";
 
 function App() {
-    const products = [
-        { name: "Product A", price: 20 }, // item
-        { name: "Product B", price: 30 }, // item
-        { name: "Product C", price: 40 }, // item
-    ];
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        console.log();
+    }
     return (
         <>
+            <button onClick={() => handleClick}>Click</button>
+            {count}
+            <button onClick={() => setCount(count + 1)}>Click</button>
+            <hr />
             {products.map((item, index) => {
                 return <Product item={item} key={index} />;
             })}
@@ -18,4 +24,12 @@ function App() {
 
 export default App;
 
-//
+// function show(value){
+//   console.log(value)
+// }
+
+// function sum(a, b, callback){
+//   callback(a + b);
+// }
+
+// sum(10, 20, show)
