eac6bad87880af18d21b0c3c172647bf82198dc8
datlt2306 <datlt2306@gmail.com>
2024-03-08 08:21:00 +0700

    WD18204 - Buổi 3 - commit - 1
    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+    const [status, setStatus] = useState<boolean>(true);
+    return (
+        <>
+            <div
+                style={{ width: 200, height: 200, backgroundColor: status ? "red" : "blue" }}
+                onClick={() => setStatus(!status)}
+            >
+                Content
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-const root = document.getElementById('root')!;
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+const root = document.getElementById("root")!;
 
-
-function hello(){
-  return <h1>Hello</h1>
-}
-
-function Hello(): JSX.Element{
-  return <h1>Hello</h1>
-}
-const myName: string = "Dat";
-const myAge: number = 40;
-const isMarried: boolean = true;
-const myInfo: {name: string, age: number, hasChild: boolean} = {
-  name: "Dat",
-  age: 20,
-  hasChild: true
-}
-
-ReactDOM.createRoot(root).render(<>
-  <h1>My Name: {myName}</h1>
-  <h1>My Age: {myAge}</h1>
-  <h1>Status: {isMarried ? 'Đã chết': 'Ế'}</h1>
-  <h1>Info {myInfo.name} - {myInfo.age}</h1>
-  <hr />
-  <h1>Function: {hello()}</h1>
-  <h1>Component: <Hello /></h1>
-</>);
-
-
-  
+ReactDOM.createRoot(root).render(<App />);
