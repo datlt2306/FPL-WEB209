5e1a6793db808a1af72ce34f50cbc43c7e673a6f
datlt2306 <datlt2306@gmail.com>
2024-03-15 08:47:17 +0700

    WD18204 - Buổi 6

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,14 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ProductContext } from "../contexts/ProductContextProvider";
 import { IProduct } from "../interfaces/Product";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const ProductList = () => {
-    const { products, onHandleRemove } = useContext(ProductContext);
+    const { products, dispatch } = useContext(ProductContext);
     console.log(products);
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get(`http://localhost:3000/products`);
+                dispatch({ type: "SET_PRODUCTS", payload: data });
+            } catch (error) {}
+        })();
+    }, []);
     return (
         <div>
-            {products.map((item: IProduct, index: number) => (
+            {products?.value?.map((item: IProduct, index: number) => (
                 <div key={index}>
                     <div>{item.name}</div>
                     <div>{item.price}</div>
--- src/contexts/ProductContextProvider.tsx
+++ src/contexts/ProductContextProvider.tsx
@@ -1,51 +1,55 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 import { IProduct } from "../interfaces/Product";
 import axios from "axios";
 
+const initialState = {
+    value: [] as IProduct[],
+};
+
 export const ProductContext = createContext({} as any);
 
+const reducer = (state: { value: IProduct[] }, action) => {
+    switch (action.type) {
+        case "SET_PRODUCTS":
+            return {
+                value: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
 const ProductContextProvider = ({ children }: { children: React.ReactNode }) => {
-    const [products, setProducts] = useState<IProduct[]>([]);
+    const [products, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const { data } = await axios.get(`http://localhost:3000/products`);
-                setProducts(data);
-            } catch (error) {}
-        })();
-    }, []);
+    // const onHandleRemove = async (id: number) => {
+    //     try {
+    //         const { data } = await axios.delete(`http://localhost:3000/products/${id}`);
+    //         setProducts(products.filter((item) => item.id !== id));
+    //     } catch (error) {}
+    // };
 
-    const onHandleRemove = async (id: number) => {
-        try {
-            const { data } = await axios.delete(`http://localhost:3000/products/${id}`);
-            setProducts(products.filter((item) => item.id !== id));
-        } catch (error) {}
-    };
-
-    const onHandleAdd = async (product: IProduct) => {
-        try {
-            const { data } = await axios.post(`http://localhost:3000/products`, product);
-            // rerender
-            setProducts([...products, data]);
-        } catch (error) {}
-    };
-    const onHandleEdit = async (product: IProduct) => {
-        try {
-            const { data } = await axios.put(
-                `http://localhost:3000/products/${product.id}`,
-                product
-            );
-            // rerender
-            setProducts(products.map((item) => (item.id === product.id ? product : item)));
-        } catch (error) {}
-    };
+    // const onHandleAdd = async (product: IProduct) => {
+    //     try {
+    //         const { data } = await axios.post(`http://localhost:3000/products`, product);
+    //         // rerender
+    //         setProducts([...products, data]);
+    //     } catch (error) {}
+    // };
+    // const onHandleEdit = async (product: IProduct) => {
+    //     try {
+    //         const { data } = await axios.put(
+    //             `http://localhost:3000/products/${product.id}`,
+    //             product
+    //         );
+    //         // rerender
+    //         setProducts(products.map((item) => (item.id === product.id ? product : item)));
+    //     } catch (error) {}
+    // };
 
     return (
         <div>
-            <ProductContext.Provider
-                value={{ products, setProducts, onHandleRemove, onHandleEdit, onHandleAdd }}
-            >
+            <ProductContext.Provider value={{ products, dispatch }}>
                 {children}
             </ProductContext.Provider>
         </div>
