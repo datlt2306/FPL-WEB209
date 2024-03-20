1df0cbae6bc113a8ac1e6f7327ffa7f1d77538e2
datlt2306 <datlt2306@gmail.com>
2024-03-20 10:53:44 +0700

    WD18205 - Buổi 8

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { IProduct } from "./interfaces/Product";
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 function App() {
     // const [products, setProducts] = useState([]);
@@ -23,6 +23,7 @@ function App() {
     // if (isLoading) return <div>Loading...</div>;
     // if (isError) return <div>Lỗi rồi</div>;
 
+    const queryClient = useQueryClient();
     const { data, isLoading, isError } = useQuery({
         queryKey: ["PRODUCT_KEY"],
         queryFn: async () => {
@@ -30,12 +31,65 @@ function App() {
             return data;
         },
     });
+
+    const { mutate: add, isPending: isAddPending } = useMutation({
+        mutationFn: async (product: IProduct) => {
+            const { data } = await axios.post("http://localhost:3000/products", product);
+            return data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["PRODUCT_KEY"],
+            });
+        },
+    });
+    const { mutate: remove } = useMutation({
+        mutationFn: async (id: number) => {
+            const { data } = await axios.delete(`http://localhost:3000/products/${id}`);
+            return data;
+        },
+        onSuccess: () => {
+            // refetching
+            queryClient.invalidateQueries({
+                queryKey: ["PRODUCT_KEY"],
+            });
+        },
+    });
+    const { mutate: update } = useMutation({
+        mutationFn: async (product: IProduct) => {
+            const { data } = await axios.put(
+                `http://localhost:3000/products/${product.id}`,
+                product
+            );
+            return data;
+        },
+        onSuccess: () => {
+            // refetching
+            queryClient.invalidateQueries({
+                queryKey: ["PRODUCT_KEY"],
+            });
+        },
+    });
+
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Lỗi rồi</div>;
     return (
         <>
+            <button onClick={() => add({ name: "Sản phẩm mới", price: 200 })}>
+                {isAddPending ? "Adding..." : "Add"}
+            </button>
             {data.map((product: IProduct) => (
-                <div key={product.id}>{product.name}</div>
+                <div key={product.id}>
+                    {product.name}
+                    <button
+                        onClick={() =>
+                            update({ ...product, id: product.id, name: "Sản phẩm update" })
+                        }
+                    >
+                        Update
+                    </button>
+                    <button onClick={() => remove(product.id!)}>Xóa</button>
+                </div>
             ))}
             {/* <Products /> */}
         </>
