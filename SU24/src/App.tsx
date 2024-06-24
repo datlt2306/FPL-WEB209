fa339828bb02c739adb2558386c91ef5ac8a7091
datlt2306 <datlt2306@gmail.com>
2024-06-24 14:19:29 +0700

    init

---
+++ SU24/src/App.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+    return <div className="App"></div>;
+};
+
+export default App;
