24d4b2975ecee8d3dcab09ac74723f5784c8ff4f
datlt2306 <datlt2306@gmail.com>
2024-06-25 13:46:12 +0700

    Buổi 1

--- SU24/src/App.tsx
+++ SU24/src/App.tsx
@@ -1,9 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
-import "./App.css";
 
 const App = () => {
-    return <div className="App"></div>;
+    const [todos, setTodos] = useState([
+        { id: 1, title: "Học React", completed: false }, // todo
+        { id: 2, title: "Đọc sách", completed: false }, // todo
+        { id: 3, title: "Chơi game", completed: false }, //todo
+    ]);
+    return (
+        <div>
+            <ul>
+                {todos.map((todo, index) => {
+                    return (
+                        <li key={index}>
+                            {todo.title}{" "}
+                            <button
+                                onClick={() =>
+                                    window.confirm("are you sure??") &&
+                                    setTodos(todos.filter((item) => item.id !== todo.id))
+                                }
+                            >
+                                Xóa
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default App;
