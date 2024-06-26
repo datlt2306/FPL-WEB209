485f728237307212d461af1516d7d755ed0be0ae
datlt2306 <datlt2306@gmail.com>
2024-06-26 15:47:05 +0700

    Buổi 2 - WD18333

--- SU24/src/App.tsx
+++ SU24/src/App.tsx
@@ -4,18 +4,52 @@ import "./App.css";
 
 const App = () => {
     const [products, setProducts] = useState([] as any[]);
-    const [status, setStatus] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [editProductId, setEditProductId] = useState<number | null>(null);
     useEffect(() => {
-        fetch(`http://localhost:3000/products`)
-            .then((response) => response.json())
-            .then((data) => setProducts(data))
-            .catch((error) => console.error(error));
-    }, [status]);
+        (async () => {
+            try {
+                const data = await (await fetch(`http://localhost:3000/products`)).json();
+                setProducts(data);
+            } catch (error: any) {
+                setError(error.message);
+            }
+        })();
+    }, []);
+
+    const onHandleRemove = async (id: number) => {
+        const confirm = window.confirm("Are you sure?");
+        if (confirm) {
+            await fetch(`http://localhost:3000/products/${id}`, { method: "DELETE" });
+            // rerender
+            setProducts(products.filter((item) => item.id !== id));
+        }
+    };
     return (
         <div className="App">
-            <button onClick={() => setStatus(true)}></button>
+            {error && <div>{error}</div>}
+
+            <h2>Thêm</h2>
+            <form>
+                <input type="text" />
+                <button>Thêm</button>
+            </form>
+            <hr />
             {products.map((item, index) => (
-                <div key={index}>{item?.name}</div>
+                <div key={index}>
+                    {editProductId === item.id ? (
+                        <form>
+                            <input type="text" defaultValue={item.name} />
+                            <button>Save</button>
+                            <button onClick={() => setEditProductId(null)}>Cancel</button>
+                        </form>
+                    ) : (
+                        <>
+                            <span onClick={() => setEditProductId(item.id)}>{item.name}</span>
+                            <button onClick={() => onHandleRemove(item.id)}>Xóa</button>
+                        </>
+                    )}
+                </div>
             ))}
         </div>
     );
