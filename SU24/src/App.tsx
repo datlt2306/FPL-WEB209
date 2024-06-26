b1d3fd98731ea30c97c634944447c49decc7213e
datlt2306 <datlt2306@gmail.com>
2024-06-26 10:48:42 +0700

    Buổi 2 - WD18329

--- SU24/src/App.tsx
+++ SU24/src/App.tsx
@@ -1,9 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-    return <div className="App"></div>;
+    const [products, setProducts] = useState<any[]>([]);
+    const [editingProductId, setEdittingProductId] = useState<number | null>(null);
+    useEffect(() => {
+        fetch(`http://localhost:3000/products`)
+            .then((response) => response.json())
+            .then((data) => setProducts(data));
+    }, []);
+
+    const onHandleRemove = (id: number) => {
+        const confirm = window.confirm("Are you sure you want to delete this item?");
+        if (confirm) {
+            fetch(`http://localhost:3000/products/${id}`, {
+                method: "DELETE",
+            }).then(() => setProducts(products.filter((item) => item.id !== id)));
+        }
+    };
+    return (
+        <div className="App">
+            <h2>Thêm sản phẩm</h2>
+            <form>
+                <input type="text" />
+                <button>Thêm</button>
+            </form>
+            <hr />
+            {products.map((item) => (
+                <div>
+                    {editingProductId === item.id ? (
+                        <form>
+                            <input type="text" defaultValue={item.name} />
+                            <button>Save</button>
+                            <button onClick={() => setEdittingProductId(null)}>Cancel</button>
+                        </form>
+                    ) : (
+                        <>
+                            <span onClick={() => setEdittingProductId(item.id)}>{item.name}</span>
+                            <button onClick={() => onHandleRemove(item.id)}>Xóa</button>
+                        </>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default App;
