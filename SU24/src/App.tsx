c58be8ce1925741e356438fb3871fb056517e002
datlt2306 <datlt2306@gmail.com>
2024-06-26 15:23:36 +0700

    Buổi 2 - WD18333

--- SU24/src/App.tsx
+++ SU24/src/App.tsx
@@ -1,9 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-    return <div className="App"></div>;
+    const [products, setProducts] = useState([] as any[]);
+    const [status, setStatus] = useState(false);
+    useEffect(() => {
+        fetch(`http://localhost:3000/products`)
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+            .catch((error) => console.error(error));
+    }, [status]);
+    return (
+        <div className="App">
+            <button onClick={() => setStatus(true)}></button>
+            {products.map((item, index) => (
+                <div key={index}>{item?.name}</div>
+            ))}
+        </div>
+    );
 };
 
 export default App;
