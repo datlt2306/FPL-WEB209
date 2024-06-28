71d2215a0f495c11e4ed5840122229bfda1c1a21
datlt2306 <datlt2306@gmail.com>
2024-06-28 10:58:03 +0700

    Buổi 3 - WD18329

--- SU24/src/App.tsx
+++ SU24/src/App.tsx
@@ -1,50 +1,134 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+interface ITodo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 const App = () => {
-    const [products, setProducts] = useState<any[]>([]);
-    const [editingProductId, setEdittingProductId] = useState<number | null>(null);
+    const [todos, setTodos] = useState<ITodo[]>([]);
+    const [currentTodo, setCurrentTodo] = useState<ITodo | any>({});
+
+    const { register, handleSubmit, reset } = useForm();
+
     useEffect(() => {
-        fetch(`http://localhost:3000/products`)
-            .then((response) => response.json())
-            .then((data) => setProducts(data));
+        // IFFE function
+        (async () => {
+            try {
+                const response = await fetch(`http://localhost:3000/todos`);
+                const data = await response.json();
+                setTodos(data);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
     }, []);
 
-    const onHandleRemove = (id: number) => {
-        const confirm = window.confirm("Are you sure you want to delete this item?");
+    const onHandleRemove = async (id: number) => {
+        const confirm = window.confirm(`Bạn có chắc chắn muốn xóa hay không???;`);
         if (confirm) {
-            fetch(`http://localhost:3000/products/${id}`, {
-                method: "DELETE",
-            }).then(() => setProducts(products.filter((item) => item.id !== id)));
+            try {
+                await fetch(`http://localhost:3000/todos/${id}`, { method: "DELETE" });
+                // thông báo
+                alert("Xóa công việc thành công!!!");
+                // rerender
+                setTodos(todos.filter((todo) => todo.id !== id));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+    const onHandleSubmit = async (formData: any) => {
+        try {
+            const response = await fetch(`http://localhost:3000/todos`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            // alert
+            alert("Thêm công việc thành công!!!");
+            // rerender
+            setTodos([...todos, data]);
+            // reset form
+            reset();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const onHandleSave = async (data: any) => {
+        if (!currentTodo) return;
+        const newTodo = { ...currentTodo, title: data.title2 };
+
+        try {
+            const response = await fetch(`http://localhost:3000/todos/${currentTodo.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newTodo),
+            });
+            const result = await response.json();
+            // alert
+            if (response.status !== 200) return alert("Cập nhật công việc thất bại!!!");
+            alert("Cập nhật công việc thành công!!!");
+            // reset current todo
+            setCurrentTodo({});
+            // rerender
+            setTodos(todos.map((todo) => (todo.id == result.id ? result : todo)));
+        } catch (error) {
+            console.error(error);
         }
     };
     return (
         <div className="App">
-            <h2>Thêm sản phẩm</h2>
-            <form>
-                <input type="text" />
-                <button>Thêm</button>
+            <h2>Thêm công việc</h2>
+            <form onSubmit={handleSubmit(onHandleSubmit)}>
+                <input type="text" {...register("title")} />
+                <button type="submit">Thêm</button>
             </form>
             <hr />
-            {products.map((item) => (
-                <div>
-                    {editingProductId === item.id ? (
-                        <form>
-                            <input type="text" defaultValue={item.name} />
-                            <button>Save</button>
-                            <button onClick={() => setEdittingProductId(null)}>Cancel</button>
-                        </form>
-                    ) : (
-                        <>
-                            <span onClick={() => setEdittingProductId(item.id)}>{item.name}</span>
-                            <button onClick={() => onHandleRemove(item.id)}>Xóa</button>
-                        </>
-                    )}
-                </div>
-            ))}
+            <h2>Danh sách công việc</h2>
+            <ul>
+                {todos.map((todo: ITodo, index) => (
+                    <li key={index}>
+                        {currentTodo.id === todo.id ? (
+                            <form onSubmit={handleSubmit(onHandleSave)}>
+                                <input type="text" {...register("title2")} />
+                                <button type="submit">Lưu</button>
+                                <button onClick={() => setCurrentTodo({})}>Hủy</button>
+                            </form>
+                        ) : (
+                            <>
+                                <span
+                                    onClick={() => {
+                                        reset({ title2: todo.title });
+                                        setCurrentTodo(todo);
+                                    }}
+                                >
+                                    {todo.title}
+                                </span>
+                                <button onClick={() => onHandleRemove(todo.id!)}>Xóa</button>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
 
 export default App;
+
+// { id: 3, name: "C update"}
+//  [ { id: 1, name: "A" }, {id: 2, name: "B"}]
+
+// tạo 1 mảng mới
+// [ { id: 1, name: "A"}, {id: 2, name: "B update"}]
