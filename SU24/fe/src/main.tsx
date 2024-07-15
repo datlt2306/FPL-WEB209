1afd01a89f7f2250c8707e2328cb0a1ef9e479b6
datlt2306 <datlt2306@gmail.com>
2024-07-15 08:19:33 +0700

    WD18328 - Buổi 7

--- SU24/fe/src/main.tsx
+++ SU24/fe/src/main.tsx
@@ -1,17 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import CounterProvider from "./context/CounterContext.tsx";
 import "./global.css";
 import "./styles/style.scss";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <CounterProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </CounterProvider>
-    </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </QueryClientProvider>,
 );
--- SU24/fe/src/pages/(dashboard)/product/page.tsx
+++ SU24/fe/src/pages/(dashboard)/product/page.tsx
@@ -1,81 +1,94 @@
 import { IProduct } from "@/common/types/product";
-import { CountContext } from "@/context/CounterContext";
 import { getAllProducts } from "@/services/product";
-import { useContext, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 const ProductPage = () => {
-    const data = useContext(CountContext);
-    console.log(data);
-    const [products, setProducts] = useState<IProduct[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    useEffect(() => {
-        (async () => {
-            try {
-                setIsLoading(true);
-                const response = await getAllProducts();
-                const data = await response.data;
-                if (response.status !== 200) throw new Error("Error");
-                setProducts(data);
-            } catch (error) {
-                setIsError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        })();
-    }, []);
+    // Client State
+    // const [products, setProducts] = useState<IProduct[]>([]);
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [isError, setIsError] = useState(false);
+    // useEffect(() => {
+    //     (async () => {
+    //         try {
+    //             setIsLoading(true);
+    //             const response = await getAllProducts();
+    //             const data = await response.data;
+    //             if (response.status !== 200) throw new Error("Error");
+    //             setProducts(data);
+    //         } catch (error) {
+    //             setIsError(true);
+    //         } finally {
+    //             setIsLoading(false);
+    //         }
+    //     })();
+    // }, []);
+    // const removeItem = async (id: number | string) => {
+    //     const confirm = window.confirm("Bạn có chắc chắn muốn xóa không?");
+    //     if (!confirm) return;
+    //     try {
+    //         setIsLoading(true);
+    //         const response = await fetch(
+    //             `http://localhost:8080/api/products/${id}`,
+    //             { method: "DELETE" },
+    //         );
+    //         if (response.status !== 200) return alert("Xóa thất bại");
+    //         setProducts(products.filter((product) => product._id !== id));
+    //     } catch (error) {
+    //         setIsError(true);
+    //     } finally {
+    //         setIsLoading(false);
+    //     }
+    // };
+    // if (isLoading) return <div>Loading....</div>;
+    // if (isError) return <div>Error</div>;
+    // return (
+    //     <>
+    //         <h1>Quản lý sản phẩm</h1>
+    //         <table>
+    //             <thead>
+    //                 <tr>
+    //                     <th>#</th>
+    //                     <th>Tên</th>
+    //                     <th>Giá</th>
+    //                     <th></th>
+    //                 </tr>
+    //             </thead>
+    //             <tbody>
+    //                 {products &&
+    //                     products.map((item: IProduct, index) => (
+    //                         <tr key={index}>
+    //                             <td>{index + 1}</td>
+    //                             <td>{item.name}</td>
+    //                             <td>{item.price}</td>
+    //                             <td>
+    //                                 <button
+    //                                     onClick={() => removeItem(item._id!)}
+    //                                 >
+    //                                     Xóa
+    //                                 </button>
+    //                             </td>
+    //                         </tr>
+    //                     ))}
+    //             </tbody>
+    //         </table>
+    //     </>
+    // );
 
-    const removeItem = async (id: number | string) => {
-        const confirm = window.confirm("Bạn có chắc chắn muốn xóa không?");
-        if (!confirm) return;
-        try {
-            setIsLoading(true);
-            const response = await fetch(
-                `http://localhost:8080/api/products/${id}`,
-                { method: "DELETE" },
-            );
-            if (response.status !== 200) return alert("Xóa thất bại");
-            setProducts(products.filter((product) => product._id !== id));
-        } catch (error) {
-            setIsError(true);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    if (isLoading) return <div>Loading....</div>;
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["products"],
+        queryFn: async () => {
+            return await getAllProducts();
+        },
+    });
+    if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error</div>;
     return (
-        <>
-            <h1>Quản lý sản phẩm</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Tên</th>
-                        <th>Giá</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products &&
-                        products.map((item: IProduct, index) => (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                                <td>
-                                    <button
-                                        onClick={() => removeItem(item._id!)}
-                                    >
-                                        Xóa
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                </tbody>
-            </table>
-        </>
+        <div>
+            {data?.data.map((item: any) => (
+                <div key={item.id}>{item.name}</div>
+            ))}
+        </div>
     );
 };
 
