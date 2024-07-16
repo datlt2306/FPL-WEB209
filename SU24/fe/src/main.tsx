1c304597d9a321b29ee68d6ec281ea9b4c26c672
datlt2306 <datlt2306@gmail.com>
2024-07-16 12:44:00 +0700

    WD18335 - Buổi 7

--- SU24/fe/src/main.tsx
+++ SU24/fe/src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./global.css";
 import "./styles/style.scss";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </QueryClientProvider>
 );
--- SU24/fe/src/pages/(admin)/products/page.tsx
+++ SU24/fe/src/pages/(admin)/products/page.tsx
@@ -1,62 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IProduct } from "@/common/types/product";
+import instance from "@/configs/axios";
 import { deleteProduct, getAllProducts } from "@/services/product";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const ProductPage = () => {
-    const [products, setProducts] = useState<IProduct[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isError, setIsError] = useState<boolean>(false);
+    // const [products, setProducts] = useState<IProduct[]>([]);
+    // const [isLoading, setIsLoading] = useState<boolean>(false);
+    // const [isError, setIsError] = useState<boolean>(false);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                // thiết lập loading trước khi gọi API
-                setIsLoading(true);
-                // gọi API
-                const response = await getAllProducts();
-                // nếu status code khác 200 ( không thành công ) thì in ra lỗi
-                if (response.status !== 200) {
-                    console.log(`Error: ${response.status}`);
-                    return;
-                }
-                // set dữ liệu trả về vào state
-                setProducts(response.data);
-                // nếu có lỗi thì set isError thành false
-            } catch (error) {
-                setIsError(true);
-            } finally {
-                // sau khi gọi API xong thì set isLoading thành false
-                setIsLoading(false);
-            }
-        })();
-    }, []);
+    // useEffect(() => {
+    //     (async () => {
+    //         try {
+    //             // thiết lập loading trước khi gọi API
+    //             setIsLoading(true);
+    //             // gọi API
+    //             const response = await getAllProducts();
+    //             // nếu status code khác 200 ( không thành công ) thì in ra lỗi
+    //             if (response.status !== 200) {
+    //                 console.log(`Error: ${response.status}`);
+    //                 return;
+    //             }
+    //             // set dữ liệu trả về vào state
+    //             setProducts(response.data);
+    //             // nếu có lỗi thì set isError thành false
+    //         } catch (error) {
+    //             setIsError(true);
+    //         } finally {
+    //             // sau khi gọi API xong thì set isLoading thành false
+    //             setIsLoading(false);
+    //         }
+    //     })();
+    // }, []);
 
-    const removeItem = async (id: number) => {
-        const confirm = window.confirm(`Bạn có chắc chắn muốn xóa sản phẩm này không?`);
-        if (!confirm) return;
-        try {
-            const response = await deleteProduct(id);
-            if (response.status !== 200) {
-                console.log(`Error: ${response.status}`);
-                return;
-            }
-            // set lại dữ liệu sau khi xóa
-            setProducts(products.filter((item) => item.id !== id));
-        } catch (error) {
-            setIsError(true);
-        }
-    };
+    // const removeItem = async (id: number) => {
+    //     const confirm = window.confirm(`Bạn có chắc chắn muốn xóa sản phẩm này không?`);
+    //     if (!confirm) return;
+    //     try {
+    //         const response = await deleteProduct(id);
+    //         if (response.status !== 200) {
+    //             console.log(`Error: ${response.status}`);
+    //             return;
+    //         }
+    //         // set lại dữ liệu sau khi xóa
+    //         setProducts(products.filter((item) => item.id !== id));
+    //     } catch (error) {
+    //         setIsError(true);
+    //     }
+    // };
 
+    // if (isLoading) return <div>Loading...</div>;
+    // if (isError) return <div>Error</div>;
+    // query: giống GET - lấy dữ liệu
+    // mutation: POST - PUT - DELETE
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["products"],
+        queryFn: () => instance.get(`/products`),
+    });
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error</div>;
-
     return (
         <div>
-            {products &&
-                products.map((item, index) => (
+            {data?.data &&
+                data?.data.map((item: any, index: number) => (
                     <div key={index}>
                         {item.name} - {item.price}
-                        <button onClick={() => removeItem(item.id!)}>Xóa</button>
+                        {/* <button onClick={() => removeItem(item.id!)}>Xóa</button> */}
                     </div>
                 ))}
         </div>
@@ -66,3 +77,7 @@ const ProductPage = () => {
 export default ProductPage;
 
 // context + reducer
+
+// global state:
+// client state
+// server state: lưu trạng thái ở server
