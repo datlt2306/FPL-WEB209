7c971304bc3105dfb0f091f9dea7209ad49896c9
datlt2306 <datlt2306@gmail.com>
2024-09-09 15:21:51 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
-import "./App.css";
 
-function App() {
-    const [color, setColor] = useState("red");
+const App = () => {
     const [status, setStatus] = useState(false);
     return (
         <>
+            {JSON.stringify(status)}
             <button onClick={() => setStatus(!status)}>Change status</button>
             <div style={{ display: status ? "none" : "block" }}>
-                <button onClick={() => setColor("blue")}>Change color</button>
-                <div style={{ width: 200, height: 200, backgroundColor: color }}>Content</div>
+                <div style={{ width: 200, height: 200, backgroundColor: "blue" }}>Content</div>
             </div>
         </>
     );
-}
+};
 
 export default App;
