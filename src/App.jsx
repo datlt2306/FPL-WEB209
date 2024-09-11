9a949b517aaf3cde9ac0cfba43c6bbedb8e27318
datlt2306 <datlt2306@gmail.com>
2024-09-11 15:09:23 +0700

    WD18402 - Buổi 2 - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
-    const [status, setStatus] = useState(false);
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    const onHandleSubmit = (e) => {
+        // chặn sự kiện reload của form
+        e.preventDefault();
+        if (!inputValue) return;
+        // spread operator
+        setTodos([...todos, { id: Date.now(), text: inputValue, done: false }]);
+        setInputValue("");
+    };
+    const toggleTodo = (id) => {
+        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo)));
+    };
+    const onHandleRemove = (id) => {
+        setTodos(todos.filter((todo) => todo.id != id));
+    };
     return (
         <>
-            {JSON.stringify(status)}
-            <button onClick={() => setStatus(!status)}>Change status</button>
-            <div style={{ display: status ? "none" : "block" }}>
-                <div style={{ width: 200, height: 200, backgroundColor: "blue" }}>Content</div>
-            </div>
+            {JSON.stringify(todos)}
+            <form onSubmit={onHandleSubmit}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onInput={(e) => setInputValue(e.target.value)}
+                />
+                <button>Thêm</button>
+            </form>
+            <ul>
+                {todos &&
+                    todos.length > 0 &&
+                    todos.map((todo) => (
+                        <li key={todo.id}>
+                            <input type="checkbox" onChange={() => toggleTodo(todo.id)} />
+                            <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+                                {todo.text}
+                            </span>
+                            <button onClick={() => onHandleRemove(todo.id)}>Xóa</button>
+                        </li>
+                    ))}
+            </ul>
         </>
     );
 };
