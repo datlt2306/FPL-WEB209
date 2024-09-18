9ce31c820c68d6f4e32c743f8195ff1839162c59
datlt2306 <datlt2306@gmail.com>
2024-09-18 13:45:07 +0700

    WD18401 - Buổi 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import ProductList from "./components/ProductList";
-import { useQuery } from "@tanstack/react-query";
 
 function App() {
     /**
@@ -12,69 +11,6 @@ function App() {
      * 2. import QueryClientProvider client={queryClient} ở main.js
      * 3.
      */
-
-    // const [products, setProducts] = useState([]);
-    // const [product, setProduct] = useState({});
-    // const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState(null);
-    // // useEffect(() => {
-    //     (async () => {
-    //         try {
-    //             setLoading(true);
-    //             const response = await axios.get(`http://localhost:3000/products`);
-    //             if (response.status !== 200) {
-    //                 throw new Error("Có lỗi xảy ra");
-    //             }
-    //             setProducts(response.data);
-    //         } catch (error) {
-    //             setError(error);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     })();
-    // }, []);
-
-    const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["products"],
-        queryFn: async () => {
-            const { data } = await axios.get(`http://localhost:3000/products`);
-            return data;
-        },
-    });
-    const onHandleSubmit = async (e) => {
-        // e.preventDefault();
-        // try {
-        //     const response = await axios.post(`http://localhost:3000/products`, product);
-        //     if (response.status !== 201) {
-        //         throw new Error("Có lỗi xảy ra");
-        //     }
-        //     console.log(`Thêm sản phẩm thành công`);
-        //     setProducts([...products, response.data]);
-        // } catch (error) {
-        //     throw new Error(error);
-        // }
-    };
-    const onHandleChange = (e) => {
-        // const { name, value, type, checked } = e.target;
-        // setProduct({ ...product, [name]: type === "checkbox" ? checked : value });
-    };
-    // const onRemove = async (id) => {
-    //     try {
-    //         setLoading(true);
-    //         const response = await axios.delete(`http://localhost:3000/products/${id}`);
-    //         if (response.status !== 200) {
-    //             throw new Error("Có lỗi xảy ra");
-    //         }
-    //         setProducts(products.filter((product) => product.id !== id));
-    //         alert("Xóa sản phẩm thành công");
-    //     } catch (error) {
-    //         setError(error);
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // };
-    if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>{error.message}</p>;
     return (
         <>
             <Routes>
@@ -82,21 +18,22 @@ function App() {
                 <Route
                     path="/add"
                     element={
-                        <form onSubmit={onHandleSubmit}>
-                            <input type="text" name="name" onChange={onHandleChange} />
-                            <input type="number" name="price" onChange={onHandleChange} />
-                            <input type="text" name="pictureUrl" onChange={onHandleChange} />
-                            <select name="category" onChange={onHandleChange}>
-                                <option value="1">Danh mục A</option>
-                                <option value="2">Danh mục b</option>
-                            </select>
-                            <textarea name="description" onChange={onHandleChange}></textarea>
-                            <input type="checkbox" name="available" onChange={onHandleChange} />
-                            <button>Submit</button>
-                        </form>
+                        <>Thêm sản phẩm</>
+                        // <form onSubmit={onHandleSubmit}>
+                        //     <input type="text" name="name" onChange={onHandleChange} />
+                        //     <input type="number" name="price" onChange={onHandleChange} />
+                        //     <input type="text" name="pictureUrl" onChange={onHandleChange} />
+                        //     <select name="category" onChange={onHandleChange}>
+                        //         <option value="1">Danh mục A</option>
+                        //         <option value="2">Danh mục b</option>
+                        //     </select>
+                        //     <textarea name="description" onChange={onHandleChange}></textarea>
+                        //     <input type="checkbox" name="available" onChange={onHandleChange} />
+                        //     <button>Submit</button>
+                        // </form>
                     }
                 />
-                <Route path="/products" element={<ProductList products={data} />} />
+                <Route path="/products" element={<ProductList />} />
             </Routes>
         </>
     );
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,39 @@
 /* eslint-disable react/prop-types */
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
 // eslint-disable-next-line react/prop-types
-const ProductList = ({ products, onRemove }) => {
+const ProductList = () => {
+    const queryClient = useQueryClient();
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ["products"],
+        queryFn: async () => {
+            const { data } = await axios.get(`http://localhost:3000/products`);
+            return data;
+        },
+    });
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: async (id) => {
+            await axios.delete(`http://localhost:3000/products/${id}`);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["products"],
+            });
+        },
+    });
+    if (isLoading) return <p>Loading...</p>;
+    if (isError) return <p>{error.message}</p>;
+    if (isPending) return <p>Deleting...</p>;
     return (
         <div>
             <h1>Danh sách sản phẩm</h1>
-            {products.map((product) => (
+            {data.map((product) => (
                 <div key={product.id}>
                     {product.name} - {product.price}
-                    <button onClick={() => onRemove(product.id)}>Xóa</button>
+                    <button onClick={() => mutate(product.id)}>Xóa</button>
                 </div>
             ))}
         </div>
