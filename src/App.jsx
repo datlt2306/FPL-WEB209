7fff86a2422867c40891d1cc8bd1e67ace83e225
datlt2306 <datlt2306@gmail.com>
2024-09-23 14:27:43 +0700

    WD18402 - Buổi 7
    Revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { Route, Routes } from "react-router-dom";
+<<<<<<< HEAD
 import LayoutAdmin from "./layouts/LayoutAdmin";
 import Dashboard from "./pages/admin/Dashboard";
 import ProductsAdminPage from "./pages/admin/Products";
+=======
+import ProductAdd from "./components/ProductAdd";
+import ProductList from "./components/ProductList";
+import ProductEdit from "./components/ProductEdit";
+
+>>>>>>> parent of 1b115fe (WD18401 - Buổi 7)
 const App = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<h1>Home Page</h1>} />
+<<<<<<< HEAD
                 <Route path="/admin" element={<LayoutAdmin />}>
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="products" element={<ProductsAdminPage />} />
                 </Route>
+=======
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/products/add" element={<ProductAdd />} />
+                <Route path="/products/:id/edit" element={<ProductEdit />} />
+>>>>>>> parent of 1b115fe (WD18401 - Buổi 7)
             </Routes>
         </>
     );
