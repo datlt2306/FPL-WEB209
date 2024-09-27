f03b27bf8b6a26441cd1a00e6bc5f1b4c48545be
datlt2306 <datlt2306@gmail.com>
2024-09-27 14:51:56 +0700

    WD18402 - Buổi 9

--- src/pages/admin/Products.jsx
+++ src/pages/admin/Products.jsx
@@ -1,76 +1,11 @@
-import React from "react";
-import { Button, Image, Skeleton, Space, Table, Tag } from "antd";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, Image, message, Popconfirm, Skeleton, Space, Table } from "antd";
 import axios from "axios";
-//  "name": "2131 update 4 5 11111",
-//       "category": "2",
-//       "price": "11111",
-//       "imageUrl": "11111",
-//       "description": "1111",
-//       "available": false,
-//       "id": 1,
-//       "status": "reuse"
-const columns = [
-    {
-        title: "Ảnh",
-        dataIndex: "imageUrl",
-        key: "imageUrl",
-        render: (_, item) => {
-            return <Image width={50} src={item.imageUrl} />;
-        },
-    },
-    {
-        title: "Tên sản phẩm",
-        dataIndex: "name",
-        key: "name",
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: "Danh mục",
-        dataIndex: "category",
-        key: "category",
-    },
-
-    {
-        title: "Giá",
-        key: "price",
-        dataIndex: "price",
-    },
-    {
-        title: "Tình trạng",
-        dataIndex: "available",
-        key: "available",
-        render: (_, item) => {
-            return <span>{item.available ? "Còn hàng" : "Hết hàng"}</span>;
-        },
-    },
-    {
-        title: "Loại hàng",
-        key: "type",
-        dataIndex: "type",
-        render: (_, item) => {
-            return <span>{item.status ? "Đã qua sử dụng" : "Mới"}</span>;
-        },
-    },
-    {
-        key: "action",
-        render: () => (
-            <>
-                <Space width="150">
-                    <Button variant="solid" color="danger">
-                        Xóa
-                    </Button>
-                    <Button variant="solid" color="primary">
-                        Cập nhật
-                    </Button>
-                </Space>
-            </>
-        ),
-    },
-];
 
 const AdminProductsPage = () => {
-    const { data, isLoading, isError, error } = useQuery({
+    const queryClient = useQueryClient();
+    const [messageApi, contextHolder] = message.useMessage();
+    const { data, isLoading } = useQuery({
         queryKey: ["products"],
         queryFn: async () => {
             const response = await axios.get(`http://localhost:3000/products`);
@@ -80,15 +15,105 @@ const AdminProductsPage = () => {
             }));
         },
     });
+    const { mutate } = useMutation({
+        mutationFn: async (id) => {
+            return await axios.delete(`http://localhost:3000/products/${id}`);
+        },
+        onSuccess: () => {
+            messageApi.success("Xóa sản phẩm thành công");
+            queryClient.invalidateQueries({
+                queryKey: ["products"],
+            });
+        },
+        onError: (error) => {
+            messageApi.error("Xóa sản phẩm không thành công", error.message);
+        },
+    });
+    const onHandleRemove = (id) => {
+        mutate(id);
+    };
+    const columns = [
+        {
+            title: "Ảnh",
+            dataIndex: "imageUrl",
+            key: "imageUrl",
+            render: (_, item) => {
+                return <Image width={50} src={item.imageUrl} />;
+            },
+        },
+        {
+            title: "Tên sản phẩm",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Danh mục",
+            dataIndex: "category",
+            key: "category",
+        },
+        {
+            title: "Giá",
+            key: "price",
+            dataIndex: "price",
+        },
+        {
+            title: "Tình trạng",
+            dataIndex: "available",
+            key: "available",
+            render: (_, item) => {
+                return <span>{item.available ? "Còn hàng" : "Hết hàng"}</span>;
+            },
+        },
+        {
+            title: "Loại hàng",
+            key: "type",
+            dataIndex: "type",
+            render: (type) => {
+                return <span>{type === "type1" ? "Hàng cũ" : "Hàng mới"}</span>;
+            },
+        },
+        {
+            key: "action",
+            render: (_, item) => (
+                <div className="w-20">
+                    <Space width="150">
+                        <Popconfirm
+                            title="Xóa sản phẩm"
+                            description="Bạn có chắc chắn muốn xóa sản phẩm này không?"
+                            onConfirm={() => onHandleRemove(item.id)}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <Button variant="solid" color="danger">
+                                Xóa
+                            </Button>
+                        </Popconfirm>
+                        <Button variant="solid" color="primary">
+                            Cập nhật
+                        </Button>
+                    </Space>
+                </div>
+            ),
+        },
+    ];
+
     return (
         <>
+            {contextHolder}
             <h1 className="text-3xl mb-5">Quản lý sản phẩm</h1>
-            {isLoading ? <Skeleton active /> : <Table columns={columns} dataSource={data} />}
+            <Skeleton active loading={isLoading}>
+                <Table columns={columns} dataSource={data} />
+            </Skeleton>
         </>
     );
 };
 export default AdminProductsPage;
 
-// { ProductList.map(item => {
-//     console.log(item)
-// })}
+/**
+ * Xóa sản phẩm:
+ * Bước 1: Click vào button, lấy được id sản phẩm
+ * Bước 2: Hiển thị confirm xác nhận xóa sản phẩm
+ * Bước 3: Sử useMutation để gọi API xóa sản phẩm dựa trên id vừa có
+ * Bước 4: Nếu thành công thì hiển thị message, ngược lại hiển thị message lỗi
+ * Bước 5: Cập nhật lại (refetching) danh sách sản phẩm
+ */
