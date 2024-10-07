407352d31ae58cb794c6eaa2139216f36bc67798
datlt2306 <datlt2306@gmail.com>
2024-10-07 13:44:25 +0700

    WD19201 - Buổi 13

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,27 +1,42 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+const initialState = { count: 0 };
 
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case "increment":
+            return { count: state.count + 1 };
+        case "decrement":
+            return { count: state.count - 1 };
+        case "increase": {
+            return { count: state.count + action.payload };
+        }
+        default:
+            return state;
+    }
+};
 const CartContextProvider = ({ children }) => {
-    const [cart] = useState([
-        {
-            id: 1,
-            products: [
-                {
-                    id: 1,
-                    name: "product 1",
-                    price: 100,
-                    quantity: 1,
-                },
-                {
-                    id: 2,
-                    name: "product 2",
-                    price: 200,
-                    quantity: 2,
-                },
-            ],
-        },
-    ]);
-    return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
+    const [count, dispatch] = useReducer(counterReducer, initialState);
+    // const [cart] = useState([
+    //     {
+    //         id: 1,
+    //         products: [
+    //             {
+    //                 id: 1,
+    //                 name: "product 1",
+    //                 price: 100,
+    //                 quantity: 1,
+    //             },
+    //             {
+    //                 id: 2,
+    //                 name: "product 2",
+    //                 price: 200,
+    //                 quantity: 2,
+    //             },
+    //         ],
+    //     },
+    // ]);
+    return <CartContext.Provider value={{ count, dispatch }}>{children}</CartContext.Provider>;
 };
 
 export const CartContext = React.createContext();
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from "react";
 import { CartContext } from "../context/cartContext";
 
 const CartPage = () => {
-    const cart = useContext(CartContext);
-    console.log(cart);
-    return <div>Cart</div>;
+    const { count, dispatch } = useContext(CartContext);
+    return (
+        <div>
+            {count.count}
+            <button onClick={() => dispatch({ type: "increment" })}>Click</button>
+        </div>
+    );
 };
 
 export default CartPage;
