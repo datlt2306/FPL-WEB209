c74fcc55783885f1c08cf750facbae5449ca6887
datlt2306 <datlt2306@gmail.com>
2024-10-07 15:58:03 +0700

    WD18402  - Buổi 13

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,9 +2,20 @@ import { useContext } from "react";
 import { CountContext } from "../context/CounterContext";
 
 const Counter = () => {
-    const state = useContext(CountContext);
-    console.log(state);
-    return <div>{state}</div>;
+    const { state, dispatch } = useContext(CountContext);
+    console.log(state.count);
+    return (
+        <div>
+            {state.count}
+            <div className="flex space-x-5">
+                <button onClick={() => dispatch({ type: "INCREMENT" })}>INCREMENT</button>
+                <button onClick={() => dispatch({ type: "DECREMENT" })}>DECREMENT</button>
+                <button onClick={() => dispatch({ type: "INCREASE", payload: 10 })}>
+                    INCREASE
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Counter;
--- src/context/CounterContext.jsx
+++ src/context/CounterContext.jsx
@@ -1,12 +1,27 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 export const CountContext = React.createContext();
 
+const reducer = (state, action) => {
+    console.log("action", action);
+    switch (action.type) {
+        case "INCREMENT":
+            return { count: state.count + 1 };
+        case "DECREMENT":
+            return { count: state.count - 1 };
+        case "INCREASE":
+            return { count: state.count + action.payload };
+        default:
+            return state;
+    }
+};
 const CounterContext = (props) => {
-    const [count] = useState(10);
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
     return (
         <div>
-            <CountContext.Provider value={count}>{props?.children}</CountContext.Provider>
+            <CountContext.Provider value={{ state, dispatch }}>
+                {props?.children}
+            </CountContext.Provider>
         </div>
     );
 };
